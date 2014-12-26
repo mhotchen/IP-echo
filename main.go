ab897fd0a690f9008f2edc28fa482f71a41dbdb6
Matthew Hotchen <m@mhn.me>
2014-12-26 23:41:44 +0000

    Basic IP response. Also handles X-Forwarded-For

--- main.go
+++ main.go
@@ -2,11 +2,34 @@ package main
 
 import "net/http"
 import "os"
+import "strings"
+import "fmt"
 
 func main() {
     port := os.Getenv("PORT")
     if port == "" {
         port = "8080"
     }
-    http.ListenAndServe(":" + port, http.FileServer(http.Dir(".")))
+    http.HandleFunc("/", IpResponse)
+    http.ListenAndServe(":" + port, nil)
+}
+
+func IpResponse(response http.ResponseWriter, request *http.Request) {
+    ip := request.Header.Get("X-Forwarded-For")
+    if ip == "" {
+        ip = request.RemoteAddr
+    }
+
+    colonPosition := strings.Index(ip, ":")
+    if colonPosition > -1 {
+        ip = ip[:colonPosition]
+    }
+
+    commaPosition := strings.Index(ip, ",")
+    if commaPosition > -1 {
+        ip = ip[:commaPosition]
+    }
+
+    fmt.Println(ip)
+    response.Write([]byte(ip))
 }
